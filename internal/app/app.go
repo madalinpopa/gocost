9c501005ffbebc3ca7615845ed14263f85336e12
Madalin Popa <contact@madalinpopa.com>
2025-05-31 18:26:15 +0300

    Refactor app to support multiple views

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,49 +5,88 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/madalinpopa/gocost/internal/data"
+	"github.com/madalinpopa/gocost/internal/ui"
 )
 
-type AppData struct {
-	data         *data.DataRoot
-	dataFilePath string
+type currentView int
+
+const (
+	viewMonthlyOverview currentView = iota
+)
+
+type AppViews struct {
+	monthlyModel *ui.MonthlyModel
 }
 
-type AppViews struct{}
-
 type App struct {
-	width  int
-	height int
-
-	currentMonth time.Month
-	currentYear  int
-
-	AppData
+	ui.Data
+	ui.MonthYear
 	AppViews
+	activeView currentView
 }
 
 func New(initialData *data.DataRoot, dataFilePath string) App {
 	now := time.Now()
-	currentM := now.Month()
-	currentY := now.Year()
 
 	return App{
-		currentMonth: currentM,
-		currentYear:  currentY,
-		AppData: AppData{
-			data:         initialData,
-			dataFilePath: dataFilePath,
+		Data: ui.Data{
+			Data:         initialData,
+			DataFilePath: dataFilePath,
+		},
+		MonthYear: ui.MonthYear{
+			CurrentMonth: now.Month(),
+			CurrentYear:  now.Year(),
+		},
+		AppViews: AppViews{
+			monthlyModel: new(ui.MonthlyModel),
 		},
 	}
 }
 
 func (m App) Init() tea.Cmd {
+	switch m.activeView {
+
+	case viewMonthlyOverview:
+		if m.monthlyModel != nil {
+			return m.monthlyModel.Init()
+		}
+
+	}
 	return nil
 }
 
 func (m App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+
+		switch m.activeView {
+
+		case viewMonthlyOverview:
+			switch msg.String() {
+
+			case "q":
+				return m, tea.Quit
+			}
+		}
+	}
 	return m, nil
 }
 
 func (m App) View() string {
-	return "Hello World"
+
+	var viewContent string
+
+	switch m.activeView {
+
+	case viewMonthlyOverview:
+		if m.monthlyModel != nil {
+			viewContent = m.monthlyModel.View()
+		} else {
+			viewContent = "Monthly overview loading..."
+		}
+	}
+
+	return viewContent
 }

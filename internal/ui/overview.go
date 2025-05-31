b73bfeb311aaa8e22af3ed35c34e5f5a2c8e7d74
Madalin Popa <contact@madalinpopa.com>
2025-05-31 18:24:55 +0300

    Implement Bubble Tea interface for MonthlyModel

--- internal/ui/overview.go
+++ internal/ui/overview.go
@@ -1,4 +1,21 @@
 package ui
 
+import tea "github.com/charmbracelet/bubbletea"
+
 type MonthlyModel struct {
+	Data
+	MonthYear
+	WindowSize
+}
+
+func (m MonthlyModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m MonthlyModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m MonthlyModel) View() string {
+	return "Monthly view"
 }

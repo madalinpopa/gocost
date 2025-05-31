51831b3af3ddc5b2c34ebdf354f12b79fee6d849
Madalin Popa <contact@madalinpopa.com>
2025-05-31 18:42:39 +0300

    Add CategoryGroupModel for bubbletea UI

--- internal/ui/category_group.go
+++ internal/ui/category_group.go
@@ -1,1 +1,20 @@
 package ui
+
+import tea "github.com/charmbracelet/bubbletea"
+
+type CategoryGroupModel struct {
+	Data
+	WindowSize
+}
+
+func (m CategoryGroupModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m CategoryGroupModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m CategoryGroupModel) View() string {
+	return "Category group view"
+}

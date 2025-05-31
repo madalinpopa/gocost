7d1ae9746e4a51f450e467529b9f41cee4aa1600
Madalin Popa <contact@madalinpopa.com>
2025-05-31 17:36:41 +0300

    Add terminal UI using bubbletea

--- cmd/gocost/main.go
+++ cmd/gocost/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/madalinpopa/gocost/internal/app"
 	"github.com/madalinpopa/gocost/internal/config"
+	"github.com/madalinpopa/gocost/internal/data"
+	"github.com/spf13/viper"
 )
 
 func main() {
 
 	if err := config.LoadConfig(); err != nil {
-		log.Fatalf("Error loading config file: %v", err)
+		fmt.Fprintf(os.Stderr, "Error loading config file: %v", err)
+		os.Exit(1)
+	}
+
+	dataFilePath := viper.GetString(config.DataFileField)
+	initialData, err := data.LoadData(dataFilePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading data from %s: %v", dataFilePath, err)
+		os.Exit(1)
+	}
+
+	app := app.New(initialData, dataFilePath)
+	p := tea.NewProgram(app, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running program: %v\n", err)
+		os.Exit(1)
 	}
 
 }

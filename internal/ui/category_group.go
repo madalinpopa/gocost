2aac067c2fbe91f84da6f9c400756246a0d1aa0f
Madalin Popa <contact@madalinpopa.com>
2025-06-01 17:18:40 +0300

    Add actions for manage category groups

--- internal/ui/category_group.go
+++ internal/ui/category_group.go
@@ -1,10 +1,28 @@
 package ui
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/madalinpopa/gocost/internal/data"
 )
+
+type GroupDeleteMsg struct {
+	GroupID string
+}
+
+type GroupAddMsg struct {
+	Group data.CategoryGroup
+}
+
+type GroupUpdateMsg struct {
+	Group data.CategoryGroup
+}
+
+type GroupManageCategoriesMsg struct {
+	Group data.CategoryGroup
+}
 
 type CategoryGroupModel struct {
 	Data
@@ -38,9 +56,133 @@ func (m CategoryGroupModel) Init() tea.Cmd {
 }
 
 func (m CategoryGroupModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
+
+	if m.isEditingName {
+
+		// Handle actions when editing
+		switch msg := msg.(type) {
+
+		case tea.KeyMsg:
+			switch msg.String() {
+
+			case "enter":
+				groupName := strings.TrimSpace(m.editInput.Value())
+				if groupName != "" {
+					newGroupId := GenerateID()
+					newGroup := data.CategoryGroup{
+						GroupID:    newGroupId,
+						GroupName:  groupName,
+						Categories: make([]data.Category, 0),
+					}
+					return m.blurInput(), func() tea.Msg {
+						return GroupAddMsg{Group: newGroup}
+					}
+				}
+
+			case "esc":
+				m.blurInput()
+				m.editInput.SetValue("")
+			}
+		}
+		m.editInput, cmd = m.editInput.Update(msg)
+		cmds = append(cmds, cmd)
+		return m, tea.Batch(cmds...)
+	}
+
+	switch msg := msg.(type) {
+
+	case tea.WindowSizeMsg:
+		m.Width = msg.Width
+		m.Height = msg.Height
+		return m, nil
+
+	case tea.KeyMsg:
+
+		// Handle navigation and select actions
+		switch msg.String() {
+
+		case "q", "esc":
+			return m, func() tea.Msg { return SwitchToMonthlyOverviewMsg{} }
+
+		case "j", "down":
+			if len(m.groups) > 0 {
+				m.cursor = (m.cursor + 1) % len(m.groups)
+			}
+			return m, nil
+
+		case "k", "up":
+			if len(m.groups) > 0 {
+				m.cursor--
+				if m.cursor < 0 {
+					m.cursor = len(m.groups) - 1
+				}
+			}
+			return m, nil
+
+		case "a", "n": // Add new category group name
+			m.editingIndex = -1
+			m.editInput.SetValue("")
+			m.editInput.Placeholder = "New Group Name"
+			return m.focusInput()
+
+		case "e": // Edit selected category group name
+			if len(m.groups) > 0 {
+				if m.cursor >= 0 && m.cursor < len(m.groups) {
+					m.editingIndex = m.cursor
+					m.editInput.SetValue(m.groups[m.cursor].GroupName)
+					m.editInput.Placeholder = "Edit Group Name"
+					return m.focusInput()
+				}
+
+			}
+
+		case "d": // Delete selected category group
+			if len(m.groups) > 0 {
+				if m.cursor >= 0 && m.cursor < len(m.groups) {
+					groupIDToDelete := m.groups[m.cursor].GroupID
+					return m, func() tea.Msg {
+						return GroupDeleteMsg{
+							GroupID: groupIDToDelete,
+						}
+					}
+				}
+			}
+
+		case "enter": // Manage categories for selected group
+			if len(m.groups) > 0 {
+				if m.cursor >= 0 && m.cursor < len(m.groups) {
+					selectedGroup := m.groups[m.cursor]
+					return m, func() tea.Msg {
+						return GroupManageCategoriesMsg{
+							Group: selectedGroup,
+						}
+					}
+				}
+			}
+
+		}
+		return m, nil
+	}
+
 	return m, nil
 }
 
 func (m CategoryGroupModel) View() string {
 	return "Category group view"
+}
+
+func (m CategoryGroupModel) focusInput() (tea.Model, tea.Cmd) {
+	m.isEditingName = true
+	m.editInput.Focus()
+	return m, textinput.Blink
+}
+
+func (m CategoryGroupModel) blurInput() tea.Model {
+	m.isEditingName = false
+	m.editInput.Blur()
+	m.editingIndex = -1
+	return m
 }

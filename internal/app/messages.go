6fb06e54f747d2dc37df747ab67c6a96bad881b2
Madalin Popa <contact@madalinpopa.com>
2025-06-01 18:38:23 +0300

    Add handle monthly view msg function

--- internal/app/messages.go
+++ internal/app/messages.go
@@ -1,1 +1,12 @@
 package app
+
+import tea "github.com/charmbracelet/bubbletea"
+
+func (m App) handleMonthlyViewMsg() (tea.Model, tea.Cmd) {
+	m.activeView = viewMonthlyOverview
+	if m.monthlyModel != nil {
+		updatedModel := m.monthlyModel.SetMonthYear(m.CurrentMonth, m.CurrentYear)
+		m.monthlyModel = &updatedModel
+	}
+	return m, nil
+}

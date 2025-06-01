81f939c937263def5fa624763798ff8c1c3ed22a
Madalin Popa <contact@madalinpopa.com>
2025-06-01 16:04:08 +0300

    Update categroy group view

--- internal/ui/category_group.go
+++ internal/ui/category_group.go
@@ -1,10 +1,36 @@
 package ui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/madalinpopa/gocost/internal/data"
+)
 
 type CategoryGroupModel struct {
 	Data
 	WindowSize
+
+	cursor int
+	groups []data.CategoryGroup
+
+	isEditingName bool            // True if currently editing a group name or adding new one
+	editInput     textinput.Model // Text input for the group name
+	editingIndex  int             // Index of the group being edited, -1 for new group
+}
+
+func NewCategoryGroupModel(data *data.DataRoot) *CategoryGroupModel {
+	ti := textinput.New()
+	ti.Placeholder = "Group Name"
+	ti.CharLimit = 30
+	ti.Width = 30
+	return &CategoryGroupModel{
+		Data: Data{
+			Root: data,
+		},
+		groups:       data.CategoryGroups,
+		editInput:    ti,
+		editingIndex: -1,
+	}
 }
 
 func (m CategoryGroupModel) Init() tea.Cmd {

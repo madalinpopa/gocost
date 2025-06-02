95a37e77e9a21453d9f74d728ba4a980e223335a
Madalin Popa <contact@madalinpopa.com>
2025-06-02 21:00:20 +0300

    Add handler for window resize in models view

--- internal/app/messages.go
+++ internal/app/messages.go
@@ -8,6 +8,28 @@ import (
 	"github.com/madalinpopa/gocost/internal/ui"
 )
 
+// handleModelsWindowResize updates the width and height within views
+func (m App) handleModelsWindowResize(msg tea.Msg) (tea.Model, []tea.Cmd) {
+	var cmds []tea.Cmd
+
+	if m.MonthlyModel != nil {
+		updatedMonthlyModel, moCmd := m.MonthlyModel.Update(msg)
+		if mo, ok := updatedMonthlyModel.(ui.MonthlyModel); ok {
+			m.MonthlyModel = &mo
+		}
+		cmds = append(cmds, moCmd)
+	}
+
+	if m.CategoryGroupModel != nil {
+		updatedCategoryGroupModel, cgCmd := m.CategoryGroupModel.Update(msg)
+		if cgMo, ok := updatedCategoryGroupModel.(ui.CategoryGroupModel); ok {
+			m.CategoryGroupModel = &cgMo
+		}
+		cmds = append(cmds, cgCmd)
+	}
+	return m, cmds
+}
+
 // handleMonthlyViewMsg switches the active view to the monthly overview and updates the MonthlyModel
 // with the current month and year, if it exists.
 func (m App) handleMonthlyViewMsg() (tea.Model, tea.Cmd) {

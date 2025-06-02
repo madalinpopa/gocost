6393f3f3cd038f833561d11b8da0500d7552e08c
Madalin Popa <contact@madalinpopa.com>
2025-06-02 19:35:08 +0300

    Add documentation to message handler functions

--- internal/app/messages.go
+++ internal/app/messages.go
@@ -8,6 +8,8 @@ import (
 	"github.com/madalinpopa/gocost/internal/ui"
 )
 
+// handleMonthlyViewMsg switches the active view to the monthly overview and updates the MonthlyModel
+// with the current month and year, if it exists.
 func (m App) handleMonthlyViewMsg() (tea.Model, tea.Cmd) {
 	m.activeView = viewMonthlyOverview
 	if m.MonthlyModel != nil {
@@ -17,6 +19,7 @@ func (m App) handleMonthlyViewMsg() (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+// handleGroupAddMsg handles the addition of a new category group. It updates the data model,
 func (m App) handleGroupAddMsg(msg tea.Msg) (tea.Model, tea.Cmd) {
 	if msg, ok := msg.(ui.GroupAddMsg); ok {
 
@@ -31,13 +34,18 @@ func (m App) handleGroupAddMsg(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 	return m, nil
 }
+
+// handleGroupDeleteMsg handles the deletion of a category group.
 func (m App) handleGroupDeleteMsg() (tea.Model, tea.Cmd) {
 	return m, nil
 }
+
+// handleGroupUpdateMsg handles the update of a category group.
 func (m App) handleGroupUpdateMsg() (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+// handleGroupManageCategoriesMsg handles the management of categories within a group.
 func (m App) handleGroupManageCategoriesMsg() (tea.Model, tea.Cmd) {
 	return m, nil
 }

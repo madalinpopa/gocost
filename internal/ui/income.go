040b0f8ea1caadd9493de09275215d588bf57b64
Madalin Popa <contact@madalinpopa.com>
2025-06-05 19:59:31 +0300

    Add window size handling to income model

--- internal/ui/income.go
+++ internal/ui/income.go
@@ -23,6 +23,16 @@ func (m IncomeModel) Init() tea.Cmd {
 }
 
 func (m IncomeModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	switch msg := msg.(type) {
+
+	case tea.WindowSizeMsg:
+		m.Width = msg.Width
+		m.Height = msg.Height
+		return m, nil
+
+	}
+
 	return m, nil
 }
 

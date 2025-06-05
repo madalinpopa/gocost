2a1f6da05a3cbf1c5f4c94f4ff0951b4bbb5acc2
Madalin Popa <contact@madalinpopa.com>
2025-06-05 19:16:39 +0300

    Add initial income model skeleton

--- internal/ui/income.go
+++ internal/ui/income.go
@@ -1,1 +1,17 @@
 package ui
+
+import tea "github.com/charmbracelet/bubbletea"
+
+type IncomeModel struct{}
+
+func (m IncomeModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m IncomeModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m IncomeModel) View() string {
+	return "Hello from income"
+}

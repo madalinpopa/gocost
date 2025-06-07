eb6ff973c4d17f4217ab11d535664199af79d16a
Madalin Popa <contact@madalinpopa.com>
2025-06-07 16:28:51 +0300

    Rename common.go to util.go and remove unused structs

--- internal/ui/util.go
+++ internal/ui/util.go
@@ -5,34 +5,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/madalinpopa/gocost/internal/data"
 )
-
-// AppData represents the application data.
-type AppData struct {
-	Data     *data.DataRoot
-	FilePath string
-}
-
-// MonthYear represents the current month and year.
-type MonthYear struct {
-	CurrentMonth time.Month
-	CurrentYear  int
-}
-
-// WindowSize represents the size of a window.
-type WindowSize struct {
-	Width  int
-	Height int
-}
-
-// AppViews holds all the models used by App
-type AppViews struct {
-	MonthlyModel       *MonthlyModel
-	CategoryGroupModel *CategoryGroupModel
-	IncomeModel        *IncomeModel
-	IncomeFormModel    *IncomeFormModel
-}
 
 // GetPreviousMonth returns the previous month and year given the current month and year.
 func GetPreviousMonth(year int, month time.Month) (int, time.Month) {

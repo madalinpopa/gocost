1b14ba878d42c14f3f799c312cde528ff4dfae88
Madalin Popa <contact@madalinpopa.com>
2025-06-07 18:36:20 +0300

    Add handler for income edit messages

--- internal/app/app.go
+++ internal/app/app.go
@@ -139,6 +139,9 @@ func (m App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case ui.SaveIncomeMsg:
 		return m.handleSaveIncomeMsg(msg)
 
+	case ui.EditIncomeMsg:
+		return m.handleEditIncomeMsg(msg)
+
 	case ui.GroupAddMsg:
 		return m.handleGroupAddMsg(msg)
 

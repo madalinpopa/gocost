098498478903d326800cc631b3a922b35fb99e55
Madalin Popa <contact@madalinpopa.com>
2025-06-11 10:21:08 +0300

    Fix data directory path handling in config loading

--- internal/config/config.go
+++ internal/config/config.go
@@ -42,18 +42,19 @@ func getDefaultDataDir() (string, error) {
 
 // LoadConfig loads the configuration from the default location.
 func LoadConfig() error {
-	viper.AddConfigPath(dataDir)
+
+	dataDirPath, err := getDefaultDataDir()
+	if err != nil {
+		return fmt.Errorf("failed to get data directory: %w", err)
+	}
+
+	viper.AddConfigPath(dataDirPath)
 	viper.SetConfigName("config")
 	viper.SetConfigType("json")
 
 	if err := viper.ReadInConfig(); err != nil {
 
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-
-			dataDirPath, err := getDefaultDataDir()
-			if err != nil {
-				return fmt.Errorf("failed to get data directory: %w", err)
-			}
 
 			dataFilename := filepath.Join(dataDirPath, defaultDataFilename)
 

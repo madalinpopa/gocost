c7e6c8897cb424ff1b84abe97aafa24920fe7fa3
Madalin Popa <contact@madalinpopa.com>
2025-06-21 17:05:28 +0300

    Add viewport and lipgloss dependencies

--- internal/ui/category.go
+++ internal/ui/category.go
@@ -6,7 +6,9 @@ import (
 	"time"
 
 	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/madalinpopa/gocost/internal/domain"
 )
 

d170187518731ebbe57718de84b858ad479cb54e
Madalin Popa <contact@madalinpopa.com>
2025-06-22 17:48:10 +0300

    Add viewport bubble import for UI overview

--- internal/ui/overview.go
+++ internal/ui/overview.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/madalinpopa/gocost/internal/config"
